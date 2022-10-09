df63357062c6c1968ce18d7a12515529ae4f43dd
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2022-10-09 11:28:01 -0300

    Exlcuido as classes teste

--- src/br/com/lojaveiculo/view/ConsultaVeiculo.java
+++ src/br/com/lojaveiculo/view/ConsultaVeiculo.java
@@ -15,6 +15,9 @@ public class ConsultaVeiculo extends javax.swing.JFrame {
      */
     public ConsultaVeiculo() {
         initComponents();
+        
+        // Adicionar painel ao fundo
+        
     }
 
     /**
@@ -26,17 +29,17 @@ public class ConsultaVeiculo extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 843, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 506, Short.MAX_VALUE)
         );
 
         pack();
