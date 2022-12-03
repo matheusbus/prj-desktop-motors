693c4232b8810b38adedb502c193df5121d1d1df
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2022-12-03 09:52:21 -0300

    Controladores base adicionados

---
+++ src/br/com/lojaveiculo/controller/BaseCadastroController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.lojaveiculo.controller;
+
+/**
+ *
+ * @author Matheus
+ */
+public abstract class BaseCadastroController extends BaseController{
+    
+    public abstract boolean verificaCamposNulos();
+    
+}
---
+++ src/br/com/lojaveiculo/controller/BaseConsultaController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.lojaveiculo.controller;
+
+/**
+ *
+ * @author Matheus
+ */
+public abstract class BaseConsultaController extends BaseController{
+    
+    //  Método chamado no controlador para popular as tabelas nas telas de consulta.
+    public abstract void popularTabela();
+    
+}
